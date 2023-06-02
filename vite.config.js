0bb1d3e318ddee94ceec51088c3f287b15734815
Graham <g_hworth@yahoo.ca>
2023-06-02 17:45:03 -0400

    2nd Commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/https://github.com/hollins4/chatgpt-app-gh",
   plugins: [react()],
 })
